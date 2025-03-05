1e238e3171f67164df8cce717f534a5f2c7727a9
MauriNestor <mauric845apaza@gmail.com>
2025-03-04 23:12:40 -0400

    update IU with new content

--- app/src/main/java/org/scesi/esqueleto/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/org/scesi/esqueleto/ui/screen/home/HomeScreen.kt
@@ -1,39 +1,52 @@
 package org.scesi.esqueleto.ui.screen.home
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 import org.koin.androidx.compose.koinViewModel
+import org.scesi.domain.models.teams.CourseSchedule
 import org.scesi.esqueleto.ui.navigation.NavFeature
 
 @Composable
-fun HomeScreen(navController: NavController ) {
+fun HomeScreen(navController: NavController) {
     val viewModel: HomeViewModel = koinViewModel()
-    val teams by viewModel.teams.collectAsState()
+    val schedules by viewModel.schedules.collectAsState()
 
-
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
         Button(onClick = { navController.navigate(NavFeature.Detail) }) {
             Text("Go to Screen B")
         }
-        Text(text = "Lista de Equipos", style = MaterialTheme.typography.headlineMedium)
 
-        if (teams.isEmpty()) {
+        Text(text = "Lista de Horarios", style = MaterialTheme.typography.headlineMedium)
+
+        if (schedules.isEmpty()) {
             Text(text = "Cargando...", modifier = Modifier.padding(top = 16.dp))
         } else {
-            LazyColumn(modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 16.dp)) {
-                items(teams) { team ->
-                    TeamItem(teamName = team.name)
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 16.dp)
+            ) {
+                items(schedules) { schedule ->
+                    ScheduleItem(schedule)
                 }
             }
         }
@@ -41,17 +54,21 @@ fun HomeScreen(navController: NavController ) {
 }
 
 @Composable
-fun TeamItem(teamName: String) {
+fun ScheduleItem(schedule: CourseSchedule) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
-        Text(
-            text = teamName,
-            modifier = Modifier.padding(16.dp),
-            style = MaterialTheme.typography.bodyLarge
-        )
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Nivel: ${schedule.levels.joinToString { it.code }}", style = MaterialTheme.typography.bodyLarge)
+            schedule.levels.forEach { level ->
+                Text(text = "Materias:", style = MaterialTheme.typography.bodyMedium)
+                level.subjects.forEach { subject ->
+                    Text(text = "- ${subject.name}", style = MaterialTheme.typography.bodySmall)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/org/scesi/esqueleto/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/org/scesi/esqueleto/ui/screen/home/HomeViewModel.kt
@@ -7,27 +7,27 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
-import org.scesi.domain.models.teams.Team
-import org.scesi.usecases.usecases.GetTeamsUseCase
 import org.scesi.domain.models.shared.EsqueletoResult
+import org.scesi.domain.models.teams.CourseSchedule
+import org.scesi.usecases.usecases.GetScheduleUseCase
 
 @KoinViewModel
 class HomeViewModel(
-    private val getTeamsUseCase: GetTeamsUseCase
+    private val getScheduleUseCase: GetScheduleUseCase
 ) : ViewModel() {
 
-    private val _teams = MutableStateFlow<List<Team>>(emptyList())
-    val teams: StateFlow<List<Team>> = _teams.asStateFlow()
+    private val _schedules = MutableStateFlow<List<CourseSchedule>>(emptyList())
+    val schedules: StateFlow<List<CourseSchedule>> = _schedules.asStateFlow()
 
     init {
-        fetchTeams()
+        fetchSchedules()
     }
 
-    private fun fetchTeams() {
+    private fun fetchSchedules() {
         viewModelScope.launch {
-            when (val result = getTeamsUseCase()) {
+            when (val result = getScheduleUseCase()) {
                 is EsqueletoResult.Success -> {
-                    _teams.value = result.data
+                    _schedules.value = result.data
                 }
                 is EsqueletoResult.Error -> {
                 }
