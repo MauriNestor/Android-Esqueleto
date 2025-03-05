6951bdafa5a18cafa324a1a371eb83cf7d0fcd69
MauriNestor <mauric845apaza@gmail.com>
2025-03-04 23:11:41 -0400

    add new get usign api scesi

--- app/src/main/java/org/scesi/esqueleto/data/server/TeamService.kt
+++ app/src/main/java/org/scesi/esqueleto/data/server/TeamService.kt
@@ -1,9 +1,13 @@
 package org.scesi.esqueleto.data.server
 
+import org.scesi.esqueleto.data.server.models.CourseScheduleRemote
 import org.scesi.esqueleto.data.server.models.TeamRemote
 import retrofit2.http.GET
 
 interface TeamService {
     @GET("366f9b08-7d72-4b9c-adb6-828c4aaddef6")
     suspend fun getTeams(): List<TeamRemote>
+
+    @GET("FCyT/2025-01/411702.json")
+    suspend fun getSchedule(): CourseScheduleRemote
 }
--- app/src/main/java/org/scesi/esqueleto/ui/skeletonApp.kt
+++ app/src/main/java/org/scesi/esqueleto/ui/skeletonApp.kt
@@ -1,30 +1,12 @@
 package org.scesi.esqueleto.ui
 
-import android.util.Log
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import org.koin.compose.koinInject
-import org.scesi.data.dataSources.TeamRemoteDataSource
 import org.scesi.esqueleto.ui.navigation.Navigation
 import org.scesi.esqueleto.ui.theme.EsqueletoTheme
 
 @Composable
 fun SkeletonApp() {
-    val teamDataSource: TeamRemoteDataSource = koinInject()
-    LaunchedEffect(Unit) {
-        try {
-            val teams = withContext(Dispatchers.IO) {
-                teamDataSource.getTeams()
-            }
-            Log.d("API_TEST", "Equipos obtenidos: $teams") // Ver en Logcat
-        } catch (e: Exception) {
-            Log.e("API_TEST", "Error al obtener equipos", e)
-        }
-    }
-
     val appState = rememberEsqueletoState()
     EsqueletoScreen{
         Navigation(
